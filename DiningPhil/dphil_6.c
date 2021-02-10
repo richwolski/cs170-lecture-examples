ae1f8e336348385283c12557ad33906131f32f61
Rich Wolski <rich@csilvm-16.cs.ucsb.edu>
2021-02-10 09:59:50 -0800

    fixed the while loop

--- DiningPhil/dphil_6.c
+++ DiningPhil/dphil_6.c
@@ -7,6 +7,8 @@
  */
 
 
+#include <unistd.h>
+#include <stdlib.h>
 #include <stdio.h>
 #include <pthread.h>
 #include "dphil.h"
@@ -53,6 +55,7 @@ void pickup(Phil_struct *ps)
 {
   Phil *pp;
   int phil_count;
+  Dllist node;
 
   pp = (Phil *) ps->v;
   phil_count = pp->phil_count;
@@ -63,8 +66,17 @@ void pickup(Phil_struct *ps)
                      pp->state[(ps->id+(phil_count-1))%phil_count] != EATING) {
     pp->state[ps->id] = EATING;
   } else {
+    /*
+     * technically, we don't need to retest because each Phil has its own
+     * cond variable and signal must wake up at least 1.  A spurious wake up,
+     * though, would cause a problem
+     */
     dll_append(pp->q, new_jval_i(ps->id)); /* put me on the queue */
-    pthread_cond_wait(pp->cv[ps->id], pp->mon);
+    node = dll_last(pp->q);
+    while((node != dll_first(pp->q)) || (pp->state[((ps->id+1))%phil_count] == EATING) ||
+          (pp->state[(ps->id+(phil_count-1))%phil_count] == EATING)) {
+    	pthread_cond_wait(pp->cv[ps->id], pp->mon);
+    }
     dll_delete_node(pp->q->flink); /* I must be at the head of the queue */
     pp->state[ps->id] = EATING;
     test_queue(ps->v); /* signal head of the queue if neighbors aren't eating */
