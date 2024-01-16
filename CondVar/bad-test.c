7b4950d9229056844e25bb00ed3e023f647b8188
Rich Wolski <rich@csilvm-17.cs.ucsb.edu>
2024-01-16 13:32:33 -0800

    better example

--- CondVar/bad-test.c
+++ CondVar/bad-test.c
@@ -4,6 +4,7 @@
 #include <pthread.h>
 
 int Counter = 0;
+pthread_mutex_t Lock0;
 pthread_mutex_t Lock1;
 pthread_mutex_t Lock2;
 int MyTurn = 1;
@@ -13,14 +14,20 @@ void *Thread_1(void *arg)
 	int i;
 	int j;
 
-	pthread_mutex_lock(&Lock2);
+	pthread_mutex_lock(&Lock0);
 	if(MyTurn == 2) {
 		printf("Thread_1: it is NOT my turn, MyTurn: %d\n",MyTurn);
+		pthread_mutex_unlock(&Lock2); // unlock the other
+		pthread_mutex_unlock(&Lock0); // exit critical section
+		for(i=0; i < 100000; i++);
 		pthread_mutex_lock(&Lock1);
-	}
+	// awake! reenter critical section
+        	pthread_mutex_lock(&Lock0);
+		printf("Thread_1: awake! MyTurn: %d\n",MyTurn);
+	} 
 	printf("Thread_1: it IS my turn, MyTurn: %d\n",MyTurn);
 	MyTurn = 2;
-	pthread_mutex_unlock(&Lock2);
+	pthread_mutex_unlock(&Lock0);
 	return(NULL);
 }
 
@@ -29,19 +36,24 @@ void *Thread_2(void *arg)
 	int i;
 	int j;
 
-	pthread_mutex_lock(&Lock1);
+	pthread_mutex_lock(&Lock0);
 	if(MyTurn == 1) {
 		printf("Thread_2: it is NOT my turn, MyTurn: %d\n",MyTurn);
-		pthread_mutex_lock(&Lock2);
+		pthread_mutex_unlock(&Lock1); // unlock the other
+		pthread_mutex_unlock(&Lock0);
+		pthread_mutex_lock(&Lock2); // reenter critical section
+		pthread_mutex_lock(&Lock0);
+		printf("Thread_2: awake!, MyTurn: %d\n",MyTurn);
 	}
 	printf("Thread_2: it IS my turn, MyTurn: %d\n",MyTurn);
 	MyTurn = 1;
-	pthread_mutex_unlock(&Lock1);
+	pthread_mutex_unlock(&Lock0);
 	return(NULL);
 }
 
 int main(int argc, char **argv)
 {
+	pthread_mutex_init(&Lock0,NULL);
 	pthread_mutex_init(&Lock1,NULL);
 	pthread_mutex_init(&Lock2,NULL);
 	pthread_t t1;
@@ -63,6 +75,7 @@ int main(int argc, char **argv)
 		Counter++;
 	}
 
+	printf("main done\n");
 	exit(0);
 }
 
