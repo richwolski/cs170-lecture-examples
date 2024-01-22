449c2c39fd69f337f68730f7016871970efe97d0
Rich Wolski <rich@csilvm-03.cs.ucsb.edu>
2024-01-22 13:19:57 -0800

    bug that has been there

--- Semaphores/sema.c
+++ Semaphores/sema.c
@@ -42,7 +42,7 @@ void P(sema *s)
 		/*
 		 * maintain semaphore invariant
 		 */
-		while(s->waiters < (-1 * s->value)) {
+		if(s->waiters < (-1 * s->value)) {
 			s->waiters++;
 			pthread_cond_wait(&(s->wait),&(s->lock));
 			s->waiters--;
