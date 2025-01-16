70394c79747ed34d418a8677b8d556d0ff1c55ef
Destructo <rich@cs.ucsb.edu>
2025-01-16 08:12:40 -0800

    script to run lecture examples

---
+++ CondVar/run-all.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+echo "### market1 ###"
+echo "./market1 -c 1 -t 1 -q 1 -s 1 -o 100000"
+./market1 -c 1 -t 1 -q 1 -s 1 -o 100000
+
+echo "./market1 -c 1 -t 1 -q 10000 -s 1 -o 100000"
+./market1 -c 1 -t 1 -q 10000 -s 1 -o 100000
+
+echo "./market1 -c 1 -t 2 -q 1 -s 1 -o 100000"
+./market1 -c 1 -t 2 -q 1 -s 1 -o 100000
+
+echo "./market1 -c 1 -t 2 -q 10000 -s 1 -o 100000"
+./market1 -c 1 -t 2 -q 10000 -s 1 -o 100000
+
+echo "/market1 -c 2 -t 2 -q 1 -s 1 -o 100000"
+./market1 -c 2 -t 2 -q 1 -s 1 -o 100000
+
+echo "./market1 -c 2 -t 2 -q 10000 -s 1 -o 100000"
+./market1 -c 2 -t 2 -q 10000 -s 1 -o 100000
+
+echo "./market1 -c 100 -t 100 -q 100 -s 1 -o 10"
+./market1 -c 100 -t 100 -q 100 -s 1 -o 10
+
+echo " " 
+
+echo "### market2 ###"
+echo "./market2 -c 1 -t 1 -q 1 -s 1 -o 100000"
+./market2 -c 1 -t 1 -q 1 -s 1 -o 100000
+
+echo "./market2 -c 1 -t 1 -q 10000 -s 1 -o 100000"
+./market2 -c 1 -t 1 -q 10000 -s 1 -o 100000
+
+echo "./market2 -c 1 -t 2 -q 1 -s 1 -o 100000"
+./market2 -c 1 -t 2 -q 1 -s 1 -o 100000
+
+echo "./market2 -c 1 -t 2 -q 10000 -s 1 -o 100000"
+./market2 -c 1 -t 2 -q 10000 -s 1 -o 100000
+
+echo "/market2 -c 2 -t 2 -q 1 -s 1 -o 100000"
+./market2 -c 2 -t 2 -q 1 -s 1 -o 100000
+
+echo "./market2 -c 2 -t 2 -q 10000 -s 1 -o 100000"
+./market2 -c 2 -t 2 -q 10000 -s 1 -o 100000
+
+echo "./market2 -c 100 -t 100 -q 100 -s 1 -o 10"
+./market2 -c 100 -t 100 -q 100 -s 1 -o 10
+
+echo " " 
+echo "### market3 ###"
+echo "./market3 -c 1 -t 1 -q 1 -s 1 -o 100000"
+./market3 -c 1 -t 1 -q 1 -s 1 -o 100000
+
+echo "./market3 -c 1 -t 1 -q 10000 -s 1 -o 100000"
+./market3 -c 1 -t 1 -q 10000 -s 1 -o 100000
+
+echo "./market3 -c 1 -t 2 -q 1 -s 1 -o 100000"
+./market3 -c 1 -t 2 -q 1 -s 1 -o 100000
+
+echo "./market3 -c 1 -t 2 -q 10000 -s 1 -o 100000"
+./market3 -c 1 -t 2 -q 10000 -s 1 -o 100000
+
+echo "/market3 -c 2 -t 2 -q 1 -s 1 -o 100000"
+./market3 -c 2 -t 2 -q 1 -s 1 -o 100000
+
+echo "./market3 -c 2 -t 2 -q 10000 -s 1 -o 100000"
+./market3 -c 2 -t 2 -q 10000 -s 1 -o 100000
+
+echo "./market3 -c 100 -t 100 -q 100 -s 1 -o 10"
+./market3 -c 100 -t 100 -q 100 -s 1 -o 10
